976115a589fd968af1a01636c8a5dde7443ffe0f
Harkirat Singh <harkiratsingh.v1@gmail.com>
2026-06-05 14:26:34 +0530

    Day 3 collection and generics

--- backend/sms/src/main/java/com/example/sms/controller/StudentController.java
+++ backend/sms/src/main/java/com/example/sms/controller/StudentController.java
@@ -1,7 +1,11 @@
 package com.example.sms.controller;
 
 import com.example.sms.model.Student;
+
+import java.util.ArrayList;
+
 import org.springframework.web.bind.annotation.*;
+import java.util.ArrayList;
 
 @RestController
 @RequestMapping("/students")
@@ -11,8 +15,12 @@ import org.springframework.web.bind.annotation.*;
 public class StudentController{
 
     @GetMapping
-    public Student getStudent(){
-        return new Student( 1,"Harkirat Singh", "Computer Science");    
+    public ArrayList<Student> getStudents(){
+        ArrayList<Student> students=new ArrayList<>();
+        students.add(new Student(1, "Harkirat Singh", "Btech"));
+        students.add(new Student(2, "Prabhjot", "MCA"));
+        students.add(new Student(3, "Aman", "BTech"));
+        return students;
     }
 
 }

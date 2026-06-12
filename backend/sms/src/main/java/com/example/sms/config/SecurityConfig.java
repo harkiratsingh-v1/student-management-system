1910b85cd9b1c07a36780c0e8ca8d585eb3d6bae
Harkirat Singh <harkiratsingh.v1@gmail.com>
2026-06-12 16:26:50 +0530

    Day 9 Spring Security Basics done with login securityDTO and securityconfigure

---
+++ backend/sms/src/main/java/com/example/sms/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.sms.config;
+import org.springframework.context.annotation.*;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception 
+    {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                                .requestMatchers("/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                );
+        return http.build();
+    }
+}
